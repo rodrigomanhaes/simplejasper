c9aaca890d97943f61e6d0cc4655c09d245ea2d2
Rodrigo Manhães <rmanhaes@gmail.com>
2021-04-09 10:03:57 -0300

    Ignore attributes with null values

--- src/main/java/simplejasper/endpoint/Generate.java
+++ src/main/java/simplejasper/endpoint/Generate.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import org.json.JSONArray;
 import org.json.XML;
 
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -18,22 +19,27 @@ import io.javalin.http.Context;
 import simplejasper.Jasper;
 
 public class Generate implements Endpoint {
-  
+    private static ObjectMapper mapper = new ObjectMapper();
+
+    static {
+      mapper.setSerializationInclusion(Include.NON_NULL);
+    }
+
     @Override
-    @SuppressWarnings("unchecked")
     public void configure(Javalin app, String basePath) {
         app.post(basePath + "/generate", (ctx) -> {
             ctx.header("Content-Type", "application/json");
             Map<String, Object> decodedData = decodedData(ctx);
             String jsonData = jsonData(decodedData);
             String xmlData = xmlData(jsonData);
+            @SuppressWarnings("unchecked")
             Map<String, Object> parameters = (Map<String, Object>) decodedData.get("parameters");
             byte[] pdf = Jasper.generate(decodedData.get("name").toString(), xmlData, parameters);
             String encodedPdf = encode64(pdf);
             ctx.result("{\"content\":\"" + encodedPdf + "\"}");
         });
     }
- 
+
     private Map<String, Object> decodedData(Context ctx) {
         Map<String, Object> requestData = parseJSON(ctx.body());
         String encodedData = (String) requestData.get("data");
@@ -43,7 +49,7 @@ public class Generate implements Endpoint {
 
     private String jsonData(Map<String, Object> requestData) {
         try {
-            return new ObjectMapper().writeValueAsString(requestData.get("data"));
+            return mapper.writeValueAsString(requestData.get("data"));
         } 
         catch (JsonProcessingException e) {
             throw new RuntimeException(e);
@@ -58,5 +64,4 @@ public class Generate implements Endpoint {
           .append("</jasper>")
           .toString();
     }
-
 }
